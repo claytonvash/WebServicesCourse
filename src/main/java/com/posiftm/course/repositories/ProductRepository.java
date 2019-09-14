b00380caa6976db64a2d41ecc57f7d603bf17669
clayton Silva <clayton.iftm@gmail.com>
2019-09-14 14:25:15 -0300

    03-11 Product

---
+++ src/main/java/com/posiftm/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.posiftm.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.posiftm.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
