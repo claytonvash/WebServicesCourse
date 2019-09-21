c18b8ef3e9b5dbce1878d2fff3b4a540b4272a79
clayton Silva <clayton.iftm@gmail.com>
2019-09-21 16:34:12 -0300

    04-08 Challenge category CRUD

--- src/main/java/com/posiftm/course/services/CategoryService.java
+++ src/main/java/com/posiftm/course/services/CategoryService.java
@@ -3,26 +3,65 @@ package com.posiftm.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.posiftm.course.entities.Category;
 import com.posiftm.course.repositories.CategoryRepository;
+import com.posiftm.course.services.exceptions.DatabaseException;
+import com.posiftm.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	public List<Category> findAll(){
+
+	public List<Category> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		return obj.get();
-	
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+
+	}
+
+	public Category insert(Category obj) {
+		return repository.save(obj);
+
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+
+	}
+
+	public Category update(Long id, Category obj) {
+
+		try {
+			Category entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
+
 	}
 
 }
