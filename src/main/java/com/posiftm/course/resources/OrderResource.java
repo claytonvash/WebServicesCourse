a5902bdfd1e2a985c170336cd25045752cdcd59c
Clayton <clayton.iftm@gmail.com>
2019-12-18 04:17:57 -0300

    05-06 OrderDTO

--- src/main/java/com/posiftm/course/resources/OrderResource.java
+++ src/main/java/com/posiftm/course/resources/OrderResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.posiftm.course.entities.Order;
+import com.posiftm.course.dto.OrderDTO;
 import com.posiftm.course.services.OrderService;
 
 @RestController
@@ -21,18 +21,18 @@ public class OrderResource {
 	private OrderService service;
 	
     @GetMapping
-	public ResponseEntity<List<Order>> findAll(){
+	public ResponseEntity<List<OrderDTO>> findAll(){
 		
-		List<Order> list = service.findAll();
+		List<OrderDTO> list = service.findAll();
 		
  		return ResponseEntity.ok().body(list);
     }
  		
  	@GetMapping(value = "/{id}")
- 	public ResponseEntity<Order> findById(@PathVariable Long id){
+ 	public ResponseEntity<OrderDTO> findById(@PathVariable Long id){
  	        
- 		Order obj = service.findById(id);
+ 		OrderDTO dto = service.findById(id);
  		
- 		return ResponseEntity.ok().body(obj);
+ 		return ResponseEntity.ok().body(dto);
  	}
 }
