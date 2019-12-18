394d3724f895202ea0c467ead6182ef4f719ef19
Clayton <clayton.iftm@gmail.com>
2019-12-18 20:04:33 -0300

    05-10 Product Delete

--- src/main/java/com/posiftm/course/services/ProductService.java
+++ src/main/java/com/posiftm/course/services/ProductService.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +19,7 @@ import com.posiftm.course.entities.Category;
 import com.posiftm.course.entities.Product;
 import com.posiftm.course.repositories.CategoryRepository;
 import com.posiftm.course.repositories.ProductRepository;
+import com.posiftm.course.services.exceptions.DatabaseException;
 import com.posiftm.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -60,6 +63,17 @@ public class ProductService {
 		}catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(id);
 		}
+	}
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+
 	}
 	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
 		entity.getCategories().clear();
