29ce277ebc315f6319e091eafbf2ebb3a0707277
Clayton <clayton.iftm@gmail.com>
2019-12-18 22:51:37 -0300

    06-04 Encoding user password

--- src/main/java/com/posiftm/course/repositories/RoleRepository.java
+++ src/main/java/com/posiftm/course/repositories/RoleRepository.java
@@ -1,9 +1,14 @@
 package com.posiftm.course.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.posiftm.course.entities.OrderItem;
 import com.posiftm.course.entities.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long>{
+
+	void saveAll(List<OrderItem> asList);
 
 }
